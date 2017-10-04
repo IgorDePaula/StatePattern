e4935b43838aecad3854dfb666537e1c687deb92
Igor De Paula <principe.borodin@gmail.com>
2017-10-04 13:38:48 -0300

    Part 2

--- index.php
+++ index.php
@@ -9,28 +9,47 @@ use App\States\WaitingState;
 
 $d = new Document('RG', new WaitingState());
 
-var_dump($d);
+//var_dump($d);
 
-echo($d->getState());
-echo "\n";
+//echo($d->getState());
+//echo "\n";
 
 $d->inAnalisis();
 
-var_dump($d);
+//var_dump($d);
 
-echo($d->getState());
-echo "\n";
+//echo($d->getState());
+//echo "\n";
 
 $d->denied();
 
-var_dump($d);
+//var_dump($d);
 
-echo($d->getState());
-echo "\n";
+//echo($d->getState());
+//echo "\n";
 
 $d->approve();
 
-var_dump($d);
+//var_dump($d);
 
-echo($d->getState());
-echo "\n";
+//echo($d->getState());
+//echo "\n";
+
+/**
+ * Segunda parte
+ */
+
+$con = new PDO("mysql:host=localhost;dbname=teste", "teste", "teste");
+
+$stmt = $con->prepare('insert into docs (name, state) values (:name, :state)');
+
+$doc = new Document('RG',new WaitingState());
+
+$name = $doc->getName();
+$state = $doc->getState();
+$stmt->bindParam(':name',$name);
+
+$stmt->bindParam(':state',$state );
+
+$stmt->execute();
+
